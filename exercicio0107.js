1f80b37e108d6c7b58101691a245bca3a42ca0e1
cesareal9 <caugustorama@gmail.com>
2024-07-01 20:25:10 -0300

    atualizado

--- exercicio0107.js
+++ exercicio0107.js
@@ -3,14 +3,7 @@ const prompt = require("prompt-sync")({ sigint: true });
 let numero = prompt("Insira um numero ");
 
 function maiorNumeroPossivel(num) {
-return parseInt(
-    num
-    .toString()
-    .split("")
-    .sort((a, b) => b - a)
-    .join(""),
-    10
-);
+return parseInt(num.toString().split("").sort((a, b) => b - a).join(""),10);
 }
 const resultado = maiorNumeroPossivel(numero);
 console.log("O maior numero possivel com os algarismos fornecidos é: " + resultado
